233e6bb5f662f4d4bfe7c28ed074d5c9cbf9479b
Enzoleau <enzo.rabahi@edu.esiee.fr>
2025-11-26 23:15:55 +0100

    Faites l'effort !

--- td5/scripts/tofu/live/tofu-state/main.tf
+++ td5/scripts/tofu/live/tofu-state/main.tf
@@ -6,5 +6,5 @@ module "state" {
 source = "github.com/Enzoleau/devops-
 base//td5/scripts/tofu/modules/state-bucket"
 
-name = "BUCKET_NAME" # Replace with a unique name
+name = "BUCKET_UNIQUE_NAME" # Replace with a unique name
 }
