d457e3e9498784222ee1867960cf980e00e19b65
Enzoleau <enzo.rabahi@edu.esiee.fr>
2025-11-27 00:17:31 +0100

    Moi ou votre médecin

--- td5/scripts/tofu/live/tofu-state/backend.tf
+++ td5/scripts/tofu/live/tofu-state/backend.tf
@@ -1,9 +1,9 @@
 terraform {
     backend "s3" {
-        bucket = "YOUR_S3_BUCKET_NAME" # Replace
+        bucket = "shoumanbucket" # Replace
         key = "td5/scripts/tofu/live/tofu-state"
         region = "us-east-2" # Your AWS region
         encrypt = true
-        dynamodb_table = "YOUR_DYNAMODB_TABLE_NAME" # Replace
+        dynamodb_table = "shoumanbucket" # Replace
     }
 }
--- td5/scripts/tofu/live/tofu-state/main.tf
+++ td5/scripts/tofu/live/tofu-state/main.tf
@@ -3,8 +3,7 @@ region = "us-east-2" # Your AWS region
 }
 module "state" {
 
-source = "github.com/Enzoleau/devops-
-base//td5/scripts/tofu/modules/state-bucket"
+source = "../../modules/state-bucket"
 
-name = "BUCKET_UNIQUE_NAME" # Replace with a unique name
+name = "shoumanbucket" # Replace with a unique name
 }
